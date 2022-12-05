06a29a9a842c730530c3df5465a0a2f8be077b40
Anton <antonleo@mail.ru>
2022-12-05 13:41:54 +1000

    everything is working

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,27 +5,64 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        //test Git now
+        int i  = 0;
+        while (i < storage.length && storage[i] != null) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int i  = 0;
+        while (i < storage.length && storage[i] != null) {
+            i++;
+        }
+        storage[i] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int i  = 0;
+        while (i < storage.length && storage[i] != null && storage[i].uuid != uuid) {
+            i++;
+        }
+        return storage[i];
     }
 
     void delete(String uuid) {
+        int i  = 0;
+        while (i < storage.length && storage[i] != null && storage[i].uuid != uuid) {
+            i++;
+        }
+        int j  = 0;
+        while (j < storage.length - 1 && storage[i] != null) {
+            storage[i] = storage[i+1];
+            i++;
+        }
+        storage[i] = null;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int i  = 0;
+        while (i < storage.length && storage[i] != null) {
+            i++;
+        }
+        Resume[] stor = new Resume[i];
+        int j  = 0;
+        while (j <= i-1) {
+            stor[j] = storage[j];
+            j++;
+        }
+        return stor;
     }
 
     int size() {
-        return 0;
+        int i  = 0;
+        while (i < storage.length && storage[i] != null) {
+            i++;
+        }
+        return i;
     }
 }
