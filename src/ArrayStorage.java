27e6a9528cbfcf68496b9658406a5fb316891de6
Anton <antonleo@mail.ru>
2022-12-08 12:18:47 +1000

    second revsion

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,49 +5,42 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int currentIndex = -1;
+    int indexOfLastElement = -1;
 
     void clear() {
-        for (int i =0; i <= currentIndex; i++) {
+        for (int i = 0; i <= indexOfLastElement; i++) {
             storage[i] = null;
         }
-        currentIndex = -1;
+        indexOfLastElement = -1;
     }
 
     void save(Resume r) {
-        currentIndex++;
-        storage[currentIndex] = r;
+        indexOfLastElement++;
+        storage[indexOfLastElement] = r;
     }
 
     Resume get(String uuid) {
-        int i  = 0;
-        while (i <= currentIndex && !storage[i].uuid.equals(uuid)) {
-            i++;
+        for (Resume r : storage) {
+            if (r == null || r.uuid == uuid) return r;
         }
-        if (i == currentIndex + 1)
-            return null;
-        else
-            return storage[i];
+        return null;
     }
 
     void delete(String uuid) {
-        int i  = 0;
-        while (i <= currentIndex && !storage[i].uuid.equals(uuid)) {
-            i++;
-        }
-        if (i != currentIndex + 1) {
-            for (int j = i; j < currentIndex; j++)
-                storage[j] = storage[j + 1];
-            storage[currentIndex] = null;
-            currentIndex--;
+        int i = 0;
+        while (i <= indexOfLastElement && !storage[i].uuid.equals(uuid)) i++;
+        if (i != indexOfLastElement + 1) {
+            storage[i] = storage[indexOfLastElement];
+            storage[indexOfLastElement] = null;
+            indexOfLastElement--;
         }
     }
 
     Resume[] getAll() {
-        return Arrays.copyOf(storage,currentIndex + 1);
+        return Arrays.copyOf(storage, indexOfLastElement + 1);
     }
 
     int size() {
-        return currentIndex + 1;
+        return indexOfLastElement + 1;
     }
 }
